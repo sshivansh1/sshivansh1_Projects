016bfa10503ed6b53bf2d6a9b0e4f1ac030d6c29
Shivansh <shivansh2162001@gmail.com>
2023-05-18 23:07:06 -0600

    Another project push

---
+++ NumberGuess_Project/Shivansh_Cmpe2800_NumGuess/Client/Client.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Threading;
+using ConnLibrary;
+using SocketFrames;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+using static System.Diagnostics.Trace;
+namespace Client
+{
+    public partial class Client : Form
+    { 
+        private Socket objSocket = null;
+        private Thread myThread = null;
+        private volatile bool isListening = false;
+        
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            ConnDialog cd = new ConnDialog(true, true); //A new instance of the connection dialog
+            
+            //With all the fields in there
+            //It displays for now, if acc to the received data
+            //the connection is successful or not
+
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                objSocket = cd.Soc;
+
+                if (objSocket.Connected)
+                {
+                    isListening = true;
+                    tb_Status.Text = "Connected";
+                    btn_Connect.Enabled = false;
+                    btn_Disconnect.Enabled = true;
+                    btn_Guess.Enabled = true;
+
+                    myThread = new Thread(RXGuess);
+                    myThread.IsBackground = true;
+                    myThread.Start();
+                }
+            }
+        }
+
+        //I need to get a reply from the server 
+        //To kind of have an idea where the random chosen value is at
+        
+        private void RXGuess()
+        {
+            while (isListening)
+            {
+                try
+                {
+                    byte[] buff = new byte[1024];
+                    int iNumBytesRxED = objSocket.Receive(buff); //Receives int number of bytes and send into a buffer
+
+                    // if zero bytes are received, the other side issued a shutdown
+                    if (iNumBytesRxED == 0)
+                    {
+                        try
+                        {
+                            WriteLine("Softy disconnect");
+                        }
+                        catch (Exception ex)
+                        {
+                            WriteLine(ex.Message);
+                        }
+                        return;
+                    }
+                    //Continously receiveing data and deserializing
+                    string data = Encoding.UTF8.GetString(buff);
+                    CSocketFrame frame = JsonConvert.DeserializeObject<CSocketFrame>(data);//will have the random number
+
+                    Invoke(new Action(() =>
+                    {
+                        if (frame.iData > 0)
+                            isHigher();
+                        else if (frame.iData < 0)
+                            isLower();
+                        else if (frame.iData == 0)
+                            IsEqual();
+                    }));
+                }
+                catch(Exception ex)
+                {
+                    Trace.WriteLine(ex);
+                    Invoke(new Action(() => Disconnect()));
+                }
+            }
+        }
+
+        private void IsEqual()
+        {
+            tbar_Guess.Minimum = 1;
+            tbar_Guess.Maximum = 1000;
+            tbar_Guess.Value = tbar_Guess.Minimum;
+            lbl_MinVal.Text = tbar_Guess.Minimum.ToString();
+            lbl_MaxVal.Text = tbar_Guess.Maximum.ToString();
+            lbl_GuessVal.Text = tbar_Guess.Value.ToString();
+            tb_Status.Text = "Correct Guess!";
+        }
+
+        private void isHigher()
+        {
+            tbar_Guess.Maximum = tbar_Guess.Value - 1;
+            tbar_Guess.Value = tbar_Guess.Maximum;
+            lbl_MaxVal.Text = tbar_Guess.Maximum.ToString();
+            lbl_GuessVal.Text = tbar_Guess.Value.ToString();
+            tb_Status.Text = "Higher, try a smaller no.";
+        }
+
+        private void isLower()
+        {
+            tbar_Guess.Minimum = tbar_Guess.Value + 1;
+            tbar_Guess.Value = tbar_Guess.Minimum;
+            lbl_MinVal.Text = tbar_Guess.Minimum.ToString();
+            lbl_GuessVal.Text = tbar_Guess.Value.ToString();
+            tb_Status.Text = "Smaller, try a larger no.";
+        }
+
+        //When the guess button is clicked,
+        //current track bar value has to be sent in the form of bytes array
+        private void Btn_Guess_Click(object sender, EventArgs e)
+        {
+            var tbarValue = tbar_Guess.Value;
+
+            if (objSocket != null)
+            {
+                try
+                {
+                    CSocketFrame frame = new CSocketFrame(tbarValue);
+                    //JSON serilize and Unicode encode
+                    string sframe = JsonConvert.SerializeObject(frame);
+                    byte[] sbuff = Encoding.UTF8.GetBytes(sframe); //Will store the information as array of bytes
+
+                    //send with socket to connected target
+                    objSocket.Send(sbuff, sbuff.Count(), SocketFlags.None);
+                }
+                catch(Exception ex)
+                {
+                    Trace.WriteLine($"Send Error! : {ex}");
+                }
+
+                //byte[] data = new byte[5] { 42, 43, 4, 5, 2 }; //sample array of bytes
+                //objSocket.Send(data, 0, 5, SocketFlags.None);
+                //objSocket.Send(ms.GetBuffer(), (int)ms.Length, SocketFlags.None);
+            }
+        }
+
+
+        private void tbar_Guess_Scroll(object sender, EventArgs e)
+        {
+            var tbarValue = tbar_Guess.Value;
+            lbl_GuessVal.Text = tbarValue.ToString();
+        }
+
+
+        private void btn_Disconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Disconnect();
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+        }
+
+        private void Disconnect()
+        {
+            try
+            {
+                isListening = false;
+                objSocket.Shutdown(SocketShutdown.Both);//disable sends and/or receives on a socket. 
+                tb_Status.Text = "Disconnected!";
+                
+                tbar_Guess.Minimum = 1;
+                tbar_Guess.Maximum = 1000;
+                tbar_Guess.Value = 1;
+                lbl_MinVal.Text = tbar_Guess.Minimum.ToString();
+                lbl_MaxVal.Text = tbar_Guess.Maximum.ToString();
+                lbl_GuessVal.Text = tbar_Guess.Value.ToString();
+
+                btn_Connect.Enabled = true;
+                btn_Disconnect.Enabled = false;
+                btn_Guess.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error disconnecting, not connected: {ex.Message}");
+            }
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            btn_Connect.Enabled = true;
+            btn_Disconnect.Enabled = false; 
+            btn_Guess.Enabled = false;
+        }
+    }
+}
+
+
+/******    Using The Binary Formatter in C# *******
+           * 
+           CSocketFrame frame = new CSocketFrame(tbarValue);
+           MemoryStream ms = new MemoryStream();
+           BinaryFormatter bf = new BinaryFormatter();
+           bf.Serialize(ms, frame);
+           objSocket.Send(ms.GetBuffer(), (int)ms.Length, SocketFlags.None);
+           * ***********     End                *********
+           */
---
+++ NumberGuess_Project/Shivansh_Cmpe2800_NumGuess/Server/Server.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_RndGuessNum = new System.Windows.Forms.TextBox();
+            this.lbl_RndGuess = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_RndGuessNum
+            // 
+            this.tb_RndGuessNum.Location = new System.Drawing.Point(240, 47);
+            this.tb_RndGuessNum.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_RndGuessNum.Name = "tb_RndGuessNum";
+            this.tb_RndGuessNum.Size = new System.Drawing.Size(168, 24);
+            this.tb_RndGuessNum.TabIndex = 0;
+            this.tb_RndGuessNum.Text = "Guess Number";
+            // 
+            // lbl_RndGuess
+            // 
+            this.lbl_RndGuess.AutoSize = true;
+            this.lbl_RndGuess.Location = new System.Drawing.Point(44, 47);
+            this.lbl_RndGuess.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_RndGuess.Name = "lbl_RndGuess";
+            this.lbl_RndGuess.Size = new System.Drawing.Size(174, 18);
+            this.lbl_RndGuess.TabIndex = 1;
+            this.lbl_RndGuess.Text = "Random Guess Number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 101);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_RndGuess);
+            this.Controls.Add(this.tb_RndGuessNum);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Server";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_RndGuessNum;
+        private System.Windows.Forms.Label lbl_RndGuess;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
