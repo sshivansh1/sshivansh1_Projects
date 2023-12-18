76340d4a60fb0e8ceaaec8d4e6955153d640112b
sshivansh1 <shivansh2162001@gmail.com>
2023-12-18 01:18:54 -0700

    comments regarding functionality of the app is done.

---
+++ 9. Stopwatch Application/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    //Click Start
+        //Will start_interval the stopwatch timer
+        //and the lap timer
+        //Will change the button name to 'stop'
+    //Click Stop
+        //stop_interval for the stopwatch timer
+        //and the lap timer
+        //will change the button name to 'resume'
+    //Click Resume
+        //starts from the current time for stopwatch timer
+        //and the lap timer
+        //changes the button name to 'stop'
+    //Click Lap
+        //saves time for the lap and stopwatch timer
+        //clear_interval and then starts again for the lap timer
+    //Click Reset
+        //will clear all the timers and clean the laptime saved div
+        //Button is changed to 'lap' and 'start'
+        //all flags must be cleared
+});
